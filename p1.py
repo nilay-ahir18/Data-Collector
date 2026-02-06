8275c83f8175f7ab5b72a2b0019861c42e489709
nilay-ahir18 <nilayahir0@gmail.com>
2026-02-06 13:59:19 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,26 @@
+print("Welcome To the Pesnol Data Collector")
+
+name=input("enter your name : ")
+age=int(input("enterv your age : "))
+hight=float(input("enter your hight : "))
+fnum=int(input("enter your favrate number : "))
+curront_year=2026
+birth_year=curront_year-age
+
+print("************************************************************************************")
+
+print ("Here We Collect Some Information About Useer")
+print("************************************************************************************")
+print("                                                                                    ")
+print ("Name:",name,type(name))
+print ("Age:",name,type(name))
+print ("Hight:",name,type(name))
+print ("Favurate Number:",name,type(name))
+print("************************************************************************************")
+print("                                                                                    ")
+print("************************************************************************************")
+print(f"Your Birth Year Is Aproximately {birth_year}(Based on Your Age of {age} )")
+print("                                                                                    ")
+print("                                                                                    ")
+
+print("thank you for useing pesnol data collector , Goodbyy !!")
